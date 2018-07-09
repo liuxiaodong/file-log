3d93ccef06bfecf52b441595fcc8688db80791fa
liuxiaodong <liuxiaodong1989@gmail.com>
2018-07-09 16:02:26 +0800

    can custom format function

--- example/out1.js
+++ example/out1.js
@@ -1,5 +1,6 @@
 const FileLog = require('../')
-
+const fmt = require('util').format;
+const REG = /%s|%d|%i|%f|%j|%o|%O|%%/ig
 
 const logger = FileLog({
     level: 'verbose',
@@ -7,10 +8,58 @@ const logger = FileLog({
     writeFile: true,
     dir: './',
     name: 'name',
-    customWrite: true 
+    // customWrite: true,
+    prettyPrint: true,
+    format: function () {
+        let args = Array.prototype.splice.call(arguments, 0);
+        let firstArg = args[0];
+        let matchd, argLen;
+        if (typeof firstArg === 'string') {
+            matchd = firstArg.match(REG)
+            argLen = args.length - 1;
+            args.shift()
+        } else {
+            firstArg = '';
+            argLen = args.length
+        }
+        let fillIndex = 0;
+        if (matchd && matchd.length) {
+            fillIndex = matchd.length;
+        }
+        args.forEach((item, i) => {
+            if (i >= fillIndex) {
+                let type = Object.prototype.toString.call(args[i]);
+                type = type.replace('[object', '').replace(']', '').trim()
+                switch (type) {
+                    case 'Number':
+                        firstArg += ' %d';
+                        break;
+                    case 'String':
+                        firstArg += ' %s';
+                        break;
+                    case 'Error':
+                        firstArg += ' %s';
+                        break;
+                    default:
+                        firstArg += ' %j';
+                        break;
+                }
+            }
+        })
+        args.unshift(firstArg);
+        return fmt.apply(null, args)
+    }
 })
+let obj =  {a: 1, b: {c: 1, g: [12, 3]}, d: [123],f: null, e: undefined}
+obj.obj = obj;
+try {
+    console.log(n)
+} catch (e) {
+    logger.verbose('==', obj, e, '123', {a: 1, b: {c:1, g: [1,2,3]}}, [11,2,3,], e);
+}
 
-logger.verbose({a: 1, b: {c: 1, g: [12, 3]}, d: [123],f: null, e: undefined});
+// util.format.call(null, 'adsf %s', new Error('123'))
+
 logger.debug('debug');
 logger.info('info');
 logger.warning('warning');
